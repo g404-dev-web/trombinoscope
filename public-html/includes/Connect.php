af3fa896e3f54bbdc0384a4c79c944e1d356fba3
guattache <cheima.guattache@hotmail.fr>
2019-05-20 12:59:23 +0200

    Shayma

--- public-html/includes/Connect.php
+++ public-html/includes/Connect.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=trombinoscopemaster_mariadb_1;dbname=simplon;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=simplon;charset=utf8', 'root', '');
 }
 catch (Exception $e)
 {
