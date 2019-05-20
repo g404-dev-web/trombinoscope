dcabe94e4f5e004d72913904b2a00a9e5c283afd
emilieC42 <chabryemilie@gmail.com>
2019-05-20 11:59:06 +0200

    Emilie-Chabry

--- public-html/includes/Connect.php
+++ public-html/includes/Connect.php
@@ -1,8 +1,7 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=trombinoscopemaster_mariadb_1;dbname=simplon;charset=utf8', 'root', '');
-}
+    $bdd = new PDO('mysql:host=trombinoscopemaster_mariadb_1;dbname=simplon;charset=utf8', 'root', '');}
 catch (Exception $e)
 {
         die('Erreur : ' . $e->getMessage());
