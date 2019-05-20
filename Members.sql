87dfed39a1af417272faa5fc9a03219016fb1414
Alexandre <a.loretzin@gmail.com>
2019-05-20 10:46:10 +0200

    a

--- Members.sql
+++ Members.sql
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `Members`
 --
+USE simplon;
 
 DROP TABLE IF EXISTS `Members`;
 
@@ -56,26 +57,6 @@ COMMIT;
 --
 -- Dumping data for table `Members`
 --
-
-INSERT INTO `Members` (`id`,`name`, `lastname`, `image`, `lien`, `phrase`) VALUES
-(1,'Jérémy', 'Marconnes', 'assets/team/Jeremy_M.png', 'promo1/Marconnes-Jeremy/index.html', 'test'),
-(2,'Wilfried', 'Artus', 'assets/team/Wilfried.jpeg', 'promo1/Wilfried-Artus/index.html', 'test'),
-(3,'Marie-France', 'Gauthier', 'assets/team/MF.jpeg', 'promo1/Gauthier-MF/index.html', 'test'),
-(4,'Jennifer', 'Parratte', 'assets/team/Jen.png', 'promo1/Paratte-Jennifer/index.html', 'test'),
-(5,'Arnaud', 'Dussart', 'assets/team/Arnaud.png', 'promo1/Dussart-Arnaud/index.html', 'test'),
-(6,'Kevin', 'Sinjy', 'assets/team/Kevin.png', 'promo1/Sinjy-Kevin/index.html', 'test'),
-(7,'Jérémy', 'Gauthier', 'assets/team/Jeremy_G.png', 'promo1/Gauthier-Jeremy/index.html', 'test'),
-(8,'Alexandre', 'Loretzin', 'assets/team/Alex.jpg', 'promo1/Loretzin-Alexandre/index.html', 'test'),
-(9,'Florent', 'Ferlay', 'assets/team/Florent.png', 'promo1/Ferlay-Florent/index.html', 'test'),
-(10,'Morgan', 'Vallicelli', 'assets/team/Morgane.jpg', 'promo1/Vallicelli-Morgan/index.html', 'test'),
-(11,'Estelle', 'Grange', 'assets/team/Estelle.jpg', 'promo1/Grange-Estelle/index.html', 'test'),
-(12,'Marine', 'Gontard', 'assets/team/Marine.png', 'promo1/Gontard-Marine/index.html', 'test'),
-(13,'Tiffany', 'Gouttenoire', 'assets/team/Tiffany.png', 'promo1/Gouttenoire-Tiffany/index.html', 'test'),
-(14,'Céline', 'Guyot', 'assets/team/Celine.png', 'promo1/Guyot-Celine/index.html', 'test'),
-(15,'Yvé', 'Da Costa', 'assets/team/Yvé.png', 'promo1/Da-Costa-Yve/index.html', 'test'),
-(16,'Thomas', 'Gaillot', 'assets/team/Thomas.png', 'promo1/Gaillot-Thomas/index.html', 'test'),
-(17,'Stéphane', 'Fernandes', 'assets/team/wilfried.jpeg', 'promo1/Fernandes-Stephane/index.html', 'test'),
-(18,'loic', 'Neyron', 'assets/team/loic.png', 'promo1/Neyron-Loic/index.html', 'test');
 
 --
 -- Indexes for dumped tables
