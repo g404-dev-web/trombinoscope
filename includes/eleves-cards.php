186e9e1f1ed7060860e3f7fe8fcbcc513de3c8ed
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-05-19 17:09:19 +0200

    Add P3 Gregory Szucs profile

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -12,3 +12,15 @@
     </a>
 </div>
 <!-- /COPIER CE BLOC -->
+<div class="col-md-4 px-4 mb-4 col-6 px-lg-5" id="Szucs-Gregory">
+    <a class="img-effect" href="promo3/Szucs-Gregory">
+        <div class="card project">
+            <img class="card-img img-effect rounded-circle effect" src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-test-user.png" alt="Card image">
+            <div class="card-body p-0">
+                <p class="mt-3 mb-1">
+                    <b>Gregory Z. Szucs</b>
+                </p>
+            </div>
+        </div>
+    </a>
+</div>
