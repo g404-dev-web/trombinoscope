f0de777085b658044f6f8ad54b986c5c2c47be36
HamzaKarfa <hamza.karfa@gmail.com>
2020-05-19 16:41:40 +0200

    commit Ajout Parti Hamza Trombinoscope

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -12,3 +12,18 @@
     </a>
 </div>
 <!-- /COPIER CE BLOC -->
+
+<!-- ##### Card Hamza ##### -->
+<div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
+    <a class="img-effect" href="promo3/Hamza-Karfa">
+        <div class="card project">
+            <img class="card-img img-effect rounded-circle effect" src="./promo3/Hamza-Karfa/src/Hamza-Photo.jpg" alt="Card image">
+            <div class="card-body p-0">
+                <p class="mt-3 mb-1">
+                    <b>Hamza Karfa</b>
+                </p>
+            </div>
+        </div>
+    </a>
+</div>
+<!-- ##### /Card Hamza ##### -->
