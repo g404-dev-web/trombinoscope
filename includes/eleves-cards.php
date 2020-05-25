b9afc192269b98a8506f23c278f4abf27a279d2a
cleroux <cleroux84@gmail.com>
2020-05-25 12:41:29 +0200

    maj photo celine

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -73,7 +73,7 @@
 <div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
     <a class="img-effect" href="promo3/maureen-depresle">
         <div class="card project">
-            <img class="card-img img-effect rounded-circle effect" src="promo3/maureen-depresle/img/avatar-maureen.png" alt="Card image">
+            <img class="card-img img-effect rounded-circle effect" src="promo3/Maureen-Depresle/img/avatar-maureen.png" alt="Card image">
             <div class="card-body p-0">
                 <p class="mt-3 mb-1">
                     <b>Maureen Depresle</b>
