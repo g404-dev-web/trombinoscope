4ce6e23acea1d0dd15ba5b3b7d7bde6e2bd11c4f
cleroux <cleroux84@gmail.com>
2020-05-25 12:19:58 +0200

    maj photo celine

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -11,12 +11,11 @@
         </div>
     </a>
 </div>
-<<<<<<< HEAD
 <!-- /COPIER CE BLOC -->
 <div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
     <a class="img-effect" href="promo3/celine-leroux">
         <div class="card project">
-            <img class="card-img img-effect rounded-circle effect" src="../promo3/celine-leroux/images/CL.jpg" alt="Card image">
+            <img class="card-img img-effect rounded-circle effect" src="promo3/celine-leroux/images/CL.jpg" alt="Card image">
             <div class="card-body p-0">
                 <p class="mt-3 mb-1">
                     <b>Céline LEROUX</b>
@@ -25,6 +24,3 @@
         </div>
     </a>
 </div>
-=======
-<!-- /COPIER CE BLOC -->
->>>>>>> 3352f08d080cdad4314752b91a47d128a2d2b891
