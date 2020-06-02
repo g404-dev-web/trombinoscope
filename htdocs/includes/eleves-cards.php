1df41e5c55bb92a5dfbcc291e963d2c8b9e11f8b
Yoan Chalavon <scorpions042@gmail.com>
2020-06-02 11:59:20 +0200

    YoanTrombi

---
+++ htdocs/includes/eleves-cards.php
@@ -0,0 +1,13 @@
+
+<div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
+    <a class="img-effect" href="./promo3/Yoan CHALAVON">
+        <div class="card project">
+            <img class="card-img img-effect rounded-circle effect" src="../img/profil.jpg">
+            <div class="card-body p-0">
+                <p class="mt-3 mb-1">
+                    <b>Yoan CHALAVON</b>
+                </p>
+            </div>
+        </div>
+    </a>
+</div>
