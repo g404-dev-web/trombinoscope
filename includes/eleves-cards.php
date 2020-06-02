a519df7b19a4890b599ad939846a5575bb6c64c4
cleroux <cleroux84@gmail.com>
2020-06-02 14:06:24 +0200

    modif photo

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -30,15 +30,15 @@
 <!-- ======================= END OF ESTHER CARD ========================== -->
 
 <!-- CARD Celine -->
-
 <div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
     <a class="img-effect" href="promo3/celine-leroux">
         <div class="card project">
-            <img class="card-img img-effect rounded-circle effect" src="promo3/celine-leroux/images/CL.jpg" alt="Card image">
-            <div class="card-body p-0">
+            <img class="card-img img-effect rounded-circle effect" src="promo3/celine-leroux/images/CL.jpg"
+                 alt="portrait">
+            <div class="card-body p-0 mb-3">
                 <p class="mt-3 mb-1">
-                    <b>Céline LEROUX</b>
-                    </p>
+                    <b>Céline Leroux</b>
+                </p>
             </div>
         </div>
     </a>
