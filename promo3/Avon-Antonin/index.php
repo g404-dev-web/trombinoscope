1f8452631a60ee048284df88fdff587db7444cbc
Antonin06 <avon.antonin@gmai.com>
2020-06-02 18:50:18 +0200

    Antonin

---
+++ promo3/Avon-Antonin/index.php
@@ -0,0 +1,345 @@
+<?php
+
+    // Configurez ici votre email qui est utilisé à plusieurs endroits sur la page
+    // Le reste des infos est à changer manuellement.
+    // C'est votre page ! Faites en ce que vous voulez.
+    $email = "avon.antonin@gmail.com";
+
+?><!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- PAGE settings -->
+    <link rel="icon">
+    <title>Simplonien Test - Simplon Roanne P3</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!-- CSS dependencies -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        type="text/css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="styles.css">
+    <!-- scss
+    <link rel="stylesheet" href="styles.css">
+    -->
+    <!-- Script: Navbar on-top -->
+
+    <script src="../../js/navbar-ontop.js"></script>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand fixed-top navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="http://trombi.simplon-roanne.com">Simplon</a>
+
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbar3SupportedContent" aria-controls="navbar3SupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-center justify-content-end"
+                id="navbar3SupportedContent">
+
+                <a class="btn navbar-btn btn-outline-light"
+                    href="http://trombi.simplon-roanne.com">Retour à l'accueil</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar end -->
+    <!-- First section -->
+    <div class="py-5 text-center section-parallax filter-dark bg-fixed"
+        style="background-image: url('/assets/promo2/simplon.jpg');">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="display-4 mb-0">Antonin Avon</h1>
+                    <h3 class="my-0">Développeur Junior Full Stack</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Second Section -->
+    <div class="container-fluid text-center bg-dark">
+        <ul class="nav nav-tabs row">
+            <li class="nav-item col-4 px-0">
+                <a href="javascript:void(0);" data-target="#profile" class="nav-link h3 my-0 active"
+                    aria-controls="profile" aria-selected="true" data-toggle="tab" id="profile-tab"
+                    role="tab">Profil</a>
+            </li>
+            <li class="nav-item col-4 px-0">
+                <a href="javascript:void(0);" data-target="#projects" class="nav-link h3 my-0"
+                    aria-controls="projects" aria-selected="false" data-toggle="tab"
+                    id="projects-tab" role="tab">Projets</a>
+            </li>
+            <li class="nav-item col-4 px-0">
+                <a href="javascript:void(0);" data-target="#contact" class="nav-link h3 my-0"
+                    aria-controls="contact" aria-selected="false" data-toggle="tab" id="contact-tab"
+                    role="tab">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <!-- Third Section -->
+    <div class="bg-light py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="tab-content my-2" id="myTabContent">
+                        <!-- First tab -->
+                        <div class="tab-pane fade show active" id="profile" role="tabpanel"
+                            aria-labelledby="profile-tab">
+                            <div class="row text-center">
+                                <div class="col-12">
+                                    <h2 class="display-4">Mon profil</h2>
+                                    <hr>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <img class="img-fluid rounded-circle p-3"
+                                        src="./img/ExportProfil.jpg" alt="Card image">
+                                </div>
+                                <div class="col-8 text-left">
+                                    <h3>Développeur Web et Mobile</h3>
+                                    <p class="lead">Actuellement en formation Développeur Web et
+                                        Mobile</p>
+                                    <p>Passionné d'informatique depuis mon enfance, c'est avec réel plaisir
+                                        que j'ai intégrer la formation à Simplon Roanne.<br><br>
+                                        Diplomé d'un BAC PRO SEN (Systèmes éléctroniques numériques) et n'ayant pas pu continuer mes études
+                                        , j'ai travaillé plusieurs années dans le commerce.
+                                        <br>
+                                    </p>
+                                    <p>
+                                        Début 2020, j'ai eu l'envie d'apprendre à créer une application mobile via Flutter/Dart. J'ai donc
+                                        suivi un cours sur Udemy, qui je le reconnais, était incompréhensible.
+                                        Suite à ça et à differentes recherches, il était évident que j'avais d'abord besoin des bases dans le
+                                        développement web avant de m'attaquer à ça. Déterminé à changer de carrière professionnelle, j'ai donc postulé
+                                        à la formation Dev Web de Simplon.
+                                        <br><br>
+
+                                    </p>
+                                    <div class="blockquote">
+                                        <p class="mb-2">Citations</p>
+                                        <div class="blockquote-footer">
+                                            First, solve the problem. Then, write the code.
+                                            <cite>John Johnson</cite>
+                                        </div>
+                                    </div>
+                                    <div class="blockquote">
+                                        <div class="blockquote-footer">
+                                            Talk is cheap. Show me the code.
+                                            <cite>Linus Torvalds</cite>
+                                        </div>
+                                    </div>
+                                    <div class="blockquote">
+                                        <div class="blockquote-footer">
+                                            Knowledge is power.
+                                            <cite> Francis Bacon</cite>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-0">
+                                    <b class="lead">Liens :</b>
+                                    <div class="liens">
+                                        <a href="https://github.com/Antonin06" class="liens"
+                                            target="_blank">
+                                            <i
+                                                class="fa fa-github d-inline fa-lg mr-3 text-dark"></i>
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/antonin-avon-2a79621a7/"
+                                            class="liens" target="_blank">
+                                            <i
+                                                class="fa fa-linkedin d-inline fa-lg mr-3 text-dark"></i>
+                                        </a>
+                                        <a href="#"
+                                            class="liens" target="_blank">
+                                            <i class="fa d-inline fa-lg mr-3 text-dark"><img
+                                                    src="../../promo1/Gauthier-MF/images/logodoyoubuzz.jpeg"
+                                                    class="logo"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Second tab -->
+                        <div class="tab-pane fade" id="projects" role="tabpanel"
+                            aria-labelledby="projects-tab">
+                            <div class="row text-center">
+                                <div class="col-12">
+                                    <h2 class="display-4">Projets</h2>
+                                    <hr>
+                                </div>
+                            </div>
+                            <section class="project py-4">
+                                <!-- One project -->
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="bg-dark text-white text-center card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Instagram-clone</h4>
+                                                <p class="card-text">
+                                                    Project effectué en duo avec <a
+                                                        href="../Girard-Lucas/">Lucas Girard</a>. L'objectif était de reproduire
+                                                    les fonctionnalités principales d'Instagram.
+                                                </p>
+                                                <a class="btn btn-lg btn-outline-secondary m-2"
+                                                    href="https://github.com/Antonin06/Instagram-binome"
+                                                    target="_blank">Voir le projet</a>
+                                            </div>
+                                        </div>
+                                        <div class="bg-dark text-white text-center card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Site perso</h4>
+                                                <p class="card-text">
+                                                    En cours de developpement...
+                                                </p>
+                                                <a class="btn btn-lg btn-outline-secondary m-2"
+                                                    href="http://antoninavon.fr/"
+                                                    target="_blank">Voir le site</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-8 text-left">
+                                        <h3>Compétences acquises</h3>
+                                        <p class="lead">Créer des pages web en HTML, CSS, PHP7 et
+                                            MySQL</p>
+                                        <p>
+                                            Le mini chat est composé d'une interface en HTML, CSS et
+                                            Javascript. <br> Les messages sont dynamiquement envoyés
+                                            et affichés en PHP grâce la base de données MySQL.
+                                        </p>
+                                        <p class="lead">Utilisation de Git</p>
+                                        <p class="lead">Utilisation de Atom</p>
+                                        <h3>Projets personnels</h3>
+                                        <p>Afin d'améliorer mes compétences en <b>FrontEnd</b>
+                                            j'ai plusieurs projets pour mon site personnel, tel que:
+                                            <br>
+                                            <ul>
+                                                <li>Plusieurs templates Bootstrap.</li>
+                                                <li>Utilisation de Javascript.</li>
+                                            </ul>
+                                    </div>
+                                </div>
+                                <hr>
+                            </section>
+                        </div>
+                        <!-- Third tab -->
+                        <div class="tab-pane fade" id="contact" role="tabpanel"
+                            aria-labelledby="contact-tab">
+                            <div class="row text-center">
+                                <div class="col-12">
+                                    <h2 class="display-4">Contactez-moi</h2>
+                                    <hr>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <h3>Mes coordonnées</h3>
+                                    <ul>
+                                        <li>
+                                            <b>Email</b> : <a
+                                                href="mailto:<?=$email?>"><?=$email?></a>
+                                        </li>
+                                        <li>
+                                            <b>Téléphone</b> : <a href="tel:0698220698">06 98 22 06 98</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-8 text-left">
+                                    <h3>Envoyez un email</h3>
+                                    <!-- Contact form -->
+                                    <form method="post" action="../../api/mail.php">
+                                        <input type="hidden" name="to" value="<?=$email?>">
+                                        <div class="form-group">
+                                            <label for="email">Votre email</label>
+                                            <input type="email"
+                                                   class="form-control form-control-lg"
+                                                   placeholder="Email"
+                                                   required="required"
+                                                   name="email">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="name">Votre nom</label>
+                                            <input type="text"
+                                                   class="form-control form-control-lg"
+                                                   placeholder="Nom"
+                                                   required="required"
+                                                   name="name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message">Votre message</label>
+                                            <textarea class="form-control p-1 form-control-lg"
+                                                      rows="3"
+                                                      placeholder="Message"
+                                                      name="message"
+                                                      required="required"></textarea>
+                                        </div>
+                                        <button type="submit"
+                                            class="btn btn-secondary btn-lg">ENVOYER</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="text-white bg-dark text-center">
+        <div class="container">
+            <div class="row">
+                <div class="p-5 col-8 offset-2">
+                    <h3>
+                        <b>Smpln</b>
+                    </h3>
+                    <p class="">
+                        <a href="#" target="_blank">27 Rue Lucien Langenieux,
+                            <br>42300 Roanne
+                        </a>
+                    </p>
+                    <p class="">
+                        <a href="mailto:simplonroanne@gmail.com">simplonroanne@gmail.com</a>
+                    </p>
+                    <p class="mb-3">
+                        <a href="tel:0625250591">06 25 25 05 91</a>
+                    </p>
+                    <a href="#" target="_blank">
+                        <i class="fa fa-github d-inline fa-lg mr-3 text-white"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fa d-inline fa-lg mr-3 text-white fa-linkedin"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fa fa-facebook d-inline fa-lg mr-3 text-white"></i>
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="text-center text-muted">© Copyright 2018 Simplon Roanne - Tous
+                            droits réservés. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <!-- Script: Smooth scrolling between anchors in the same page -->
+    <script src="/js/smooth-scroll.js"></script>
+</body>
+
+</html>
