d85e3ff96814a0828fb31fc298131ff92479e658
Olivier Guillemot <leojen23@hotmail.com>
2020-06-02 13:39:25 +0200

    changement lien photo elevel card

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -18,10 +18,15 @@
 <div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
     <a class="img-effect" href="./promo3/Olivier-Guillemot">
         <div class="card project">
-            <img class="card-img img-effect rounded-circle effect" src="https://media-exp1.licdn.com/dms/image/C4D03AQHVdkIXYcnWMg/profile-displayphoto-shrink_200_200/0?e=1596672000&v=beta&t=DFKHunYkD5caK_R4Gv11MDuWjRPYgfx4XKWWp_nV02s" alt="Card image">
+            <img class="card-img img-effect rounded-circle effect" src="promo3/Olivier-Guillemot/IMG/olivier300.jpeg" alt="Card image">
             <div class="card-body p-0">
                 <p class="mt-3 mb-1">
                     <b>Olivier Guillemot</b>
+                </p>
+            </div>
+        </div>
+    </a>
+</div>
 <!-- END CARD OLIVIER GUILLEMOT -->
 
 <!-- ======================= ESTHER CARD ========================== -->
