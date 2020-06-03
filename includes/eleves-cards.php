390ba13229eacb576c23390a1998de339a5501de
Balthazard Matéo <mateo.balthazard@hotmail.com>
2020-06-03 18:08:34 +0200

    ajout trombi mat

--- includes/eleves-cards.php
+++ includes/eleves-cards.php
@@ -262,3 +262,17 @@
         </div>
     </a>
 </div>
+<!-- TROMBI MATEO -->
+<div class="col-md-4 px-4 mb-4 col-6 px-lg-5">
+    <a class="img-effect" href="promo3/Matéo-Balthazard">
+        <div class="card project">
+            <img class="card-img img-effect rounded-circle effect" src="promo3/Matéo-Balthazard/images/mateologo.jpg" alt="Card image">
+            <div class="card-body p-0">
+                <p class="mt-3 mb-1">
+                    <b>Matéo Balthazard</b>
+                </p>
+            </div>
+        </div>
+    </a>
+</div>
+<!-- TROMBI MATEO FIN -->
