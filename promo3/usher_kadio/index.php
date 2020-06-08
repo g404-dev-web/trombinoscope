b45fa30f307eea1da151dd2295e4316ff2d33c29
kadev0502 <kadev0502@gmail.com>
2020-06-08 10:57:52 +0200

    update de ma page profil

--- promo3/usher_kadio/index.php
+++ promo3/usher_kadio/index.php
@@ -167,18 +167,18 @@ $email = "kadiousherxavier@gmail.com";
                                       <p>J'ai effectué une formation de Technicien Supérieur en Automatisme qui m’as permis de connaître les bases du développement ( en C et C++).
                                        <br> <br>Suite à cette formation j’ai travaillé pendant un an  dans un bureau
                                        d’études avant que ma passion pour le développement
-                                       web ne soit plus forte .Après une rupture
+                                       web ne soit plus forte. Après une rupture
                                        conventionnelle je décide donc de me lancer
                                        pleinement dans une formation et mon choix se tourne
                                        logiquement vers l’école Simplon au vue du retour
                                        positif des anciens apprenants et après des
-                                       entretiens beaucoup fructueux avec les formateurs.
-                                       <d.
+                                       entretiens fructueux avec les formateurs.
+
                                       </p>
                                       <p>
                                           Depuis que j’ai été retenu et malgré les conditions particulières dans
-                                       lesquelles nous avons commencé cette formation
-                                       ,chaque jour en
+                                       lesquelles nous avons commencé cette
+                                          formation, chaque jour en
                                        formation me
                                        conforte
                                        dans mon
@@ -188,8 +188,9 @@ $email = "kadiousherxavier@gmail.com";
                                           Mon bon niveau d’anglais me permet dans cette formation de chercher facilement des infos lorsque j’ai besoin de me documenter.
                                       </p>
                                    <br>
-                                   <p>A part coder ,je suis un passionné de danse et de Football que je pratique
-                                    beaucoup .</p>
+                                   <p>A part coder, je suis un passionné de danse
+                                      et de Football que je pratique
+                                    beaucoup.</p>
                                      <div class = "citation text-success">
                                       <div class = "blockquote">
                                           <p class = "mb-2">Citations</p>
@@ -371,13 +372,10 @@ $email = "kadiousherxavier@gmail.com";
                                               <div class = "bg-info text-white text-center py-2">
                                                   <h3><i class = "fa fa-envelope mr-2"></i>Envoyez moi un
                                                    mail</h3>
-<<<<<<< HEAD
+
                                                   <p class = "m-0">C'est avec plaisir que je vous répondrai</p>
-=======
-                                                  <p class = "m-0">C'est avec
-                                                                   plaisir que je
-                                                                   vous répondrai</p>
->>>>>>> 2e5d281fdb2ef94d25b872862b3ccd9fdb2f0d70
+
+
                                               </div>
                                           </div>
                                           <div class = "card-body p-3">
